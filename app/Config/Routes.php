a2741708d5497e772aeb0a5bf85526a3bf5bf92f
Ali Fahmi Yusuf <107609258+alifhmiysf@users.noreply.github.com>
2023-10-17 18:33:30 +0700

    tampilan sign in, sign up, dashboard pengajuan part1

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/auth/login', 'LoginController::index');
+$routes->get('/auth/register', 'LoginController::registrasi');
+$routes->get('/admin', 'AdminController::index');
