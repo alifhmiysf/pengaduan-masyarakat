eff000140e9471a212016e2c5b24cb9276e2a4de
Ali Fahmi Yusuf <107609258+alifhmiysf@users.noreply.github.com>
2023-10-23 13:45:50 +0700

    membenarkan tampilan dashboard verifikasi

--- app/Views/admin/verifikasi.php
+++ app/Views/admin/verifikasi.php
@@ -12,12 +12,13 @@
     <title>Dashboard Admin</title>
 
     <!-- Custom fonts for this template-->
-    <link href="assets-dashboard-admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?= base_url(' assets-dashboard-admin/vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
+    <!-- <link rel="stylesheet" href="<?= base_url('css/bootstrap.min.css') ?>" />
+    <link rel="stylesheet" href="<?= base_url('css/style-register.css')?>"> -->
     <!-- Custom styles for this template-->
-    <link href="assets-dashboard-admin/css/sb-admin-2.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="assets-dashboard-admin/css/style.css">
+    <link href="<?= base_url('assets-dashboard-admin/css/sb-admin-2.min.css') ?>" rel="stylesheet">
+    <link rel="stylesheet" href="<?= base_url('assets-dashboard-admin/css/style.css') ?>">
 </head>
 
 <body id="page-top">
@@ -210,14 +211,14 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="assets-dashboard-admin/vendor/jquery/jquery.min.js"></script>
-    <script src="assets-dashboard-admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= base_url(' assets-dashboard-admin/vendor/jquery/jquery.min.js') ?>"></script>
+    <script src="<?= base_url('assets-dashboard-admin/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="assets-dashboard-admin/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?= base_url(' assets-dashboard-admin/vendor/jquery-easing/jquery.easing.min.js') ?>"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="assets-dashboard-admin/js/sb-admin-2.min.js"></script>
+    <script src="<?=  base_url(' assets-dashboard-admin/js/sb-admin-2.min.js')?>"></script>
 
 </body>
 
