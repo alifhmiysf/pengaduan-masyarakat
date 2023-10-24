7746131a354474390d1bb2b818d01f737e37bfa1
kinar aurasae <88175220+kinzx@users.noreply.github.com>
2023-10-24 12:56:36 +0700

    membenarkan navbar

--- app/Views/users/index.php
+++ app/Views/users/index.php
@@ -25,7 +25,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    
+
     <!--
 
 TemplateMo 590 topic listing
@@ -36,28 +36,23 @@ https://templatemo.com/tm-590-topic-listing
 </head>
 
 <body id="top">
-<h1>ASUUU</h1>
     <main>
-        <nav class="navbar navbar-expand-lg  ">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Features</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Pricing</a>
-                        </li>
-                    </ul>
+                <a class="navbar-brand" href="#">Pengaduan</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link" href="#">Features</a>
+                        <a class="nav-link" href="#">Pricing</a>
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </div>
                 </div>
             </div>
         </nav>
-
 
         <section class="hero-section d-flex justify-content-center align-items-center" id="section_1">
             <div class="container">
@@ -65,7 +60,6 @@ https://templatemo.com/tm-590-topic-listing
 
                     <div class="col-lg-8 col-12 mx-auto">
                         <h4 class="text-white text-center">Layanan Aspirasi dan Pengaduan Online Rakyat</h4>
-
                         <h6 class="text-center">Sampaikan laporan Anda langsung kepada instansi pemerintah berwenang</h6>
                     </div>
                 </div>
@@ -79,7 +73,11 @@ https://templatemo.com/tm-590-topic-listing
 
                     <div class="col-lg-9 col-12 mb-4 mb-lg-0">
                         <div class="custom-block bg-white shadow-lg">
-                            
+                            <form action="">
+                                <div class="form-group">
+                                    
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
