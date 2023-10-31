2447718608b8ac72c651a2948c8cac8117cce116
kinar aurasae <88175220+kinzx@users.noreply.github.com>
2023-10-31 10:36:42 +0700

    tombil logout

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -90,16 +90,25 @@
                     </form>
 
                     <!-- Topbar Search -->
-                    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                        <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="button">
-                                    <i class="fas fa-search fa-sm"></i>
-                                </button>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col d-grid gap-2 d-md-flex justify-content-md-end ">
+                                <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                                        <div class="input-group-append">
+                                            <button class="btn btn-primary" type="button">
+                                                <i class="fas fa-search fa-sm"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
                         </div>
-                    </form>
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-end col">
+                            <a class="btn btn-outline-dark" href="<?= base_url('/auth/login') ?>" role="button">Log Out</a>
+                        </div>
+                    </div>
 
                     <!-- Topbar Navbar -->
 
