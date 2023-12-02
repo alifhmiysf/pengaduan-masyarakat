332d5e68af49c75a89c1e79c3ace9390b9d63e33
Ali Fahmi Yusuf <107609258+alifhmiysf@users.noreply.github.com>
2023-12-02 23:04:09 +0700

    merapikan 3

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,11 +7,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->group('auth', function ($routes) {
-    // Rute Registrasi
     $routes->get('register', 'LoginController::registrasi');
     $routes->post('register', 'LoginController::valid_register');
-
-    // Rute Login dan Otentikasi
     $routes->get('login', 'LoginController::index');
     $routes->post('login', 'LoginController::valid_login');
     $routes->get('logout', 'LoginController::logout');
@@ -22,22 +19,17 @@ $routes->group('admin', function ($routes) {
     $routes->get('verifikasi', 'AdminController::verval');
     $routes->get('petugas', 'AdminController::petugas');
     $routes->get('manajemen_masyarakat', 'ManajemenMasyarakat::index');
-    $routes->post('tambah_tanggapan/(:any)','AdminController::tanggapan/$1');
+    $routes->post('tambah_tanggapan/(:any)', 'AdminController::tanggapan/$1');
 });
 
 $routes->group('pengaduan-masyarakat', function ($routes) {
-    // Rute Masyarakat
-    
     $routes->get('history', 'MasyarakatController::history');
     $routes->get('afterlogin', 'MasyarakatController::afterlogin');
-
-    $routes->post('afterloginn', 'PengaduanController::create'); 
-    $routes->post('afterloginn', 'PengaduanController::index'); 
-    
+    $routes->post('afterloginn', 'PengaduanController::create');
+    $routes->post('afterloginn', 'PengaduanController::index');
 });
 
 $routes->group('petugas', function ($routes) {
-    // Rute petugas
     $routes->get('/', 'PetugasController::index');
     $routes->get('masyarakat', 'PetugasController::masyarakat');
 });
@@ -45,7 +37,6 @@ $routes->get('admincontroller/terima/(:num)', 'AdminController::terima/$1');
 $routes->get('admincontroller/tolak/(:num)', 'AdminController::tolak/$1');
 $routes->post('admin/tanggapan/(:num)', 'AdminController::Tanggapan/$1');
 $routes->post('/tambahpetugas', 'PetugasController::tambahpetugas');
-$routes->add('admin/reset-password', 'admin::resetPassword');
 $routes->post('pengaduan/update', 'PengaduanController::update');
 $routes->get('/pengaduan/delete/(:num)', 'PengaduanController::delete/$1');
 
