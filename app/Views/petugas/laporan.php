2554adb1db97a99a6dc4b306e924920a3290022a
Ali Fahmi Yusuf <107609258+alifhmiysf@users.noreply.github.com>
2023-12-03 19:34:15 +0700

    benarkan dikit

--- app/Views/petugas/laporan.php
+++ app/Views/petugas/laporan.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Dashboard Admin</title>
+    <title>Dashboard Petugas</title>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
